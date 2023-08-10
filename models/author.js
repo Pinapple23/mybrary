0d96b81d543fbbd05638160affecbc0e4fb8fb80
Pinapple23 <veronika.fayez45@gmail.com>
2023-08-10 20:05:04 +0300

    tutorial6

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Book = require('./book')
 
 const authorSchema = new mongoose.Schema({
   name: {
@@ -6,5 +7,31 @@ const authorSchema = new mongoose.Schema({
     required: true
   }
 })
+
+// authorSchema.pre('deleteOne', function(next) {
+//   Book.find({ author: this.id }, (err, books) => {
+//     if (err) {
+//       next(err)
+//     } else if (books.length > 0) {
+//       next(new Error('This author has books still'))
+//     } else {
+//       next()
+//     }
+//   })
+// })
+authorSchema.pre("deleteOne", async function (next) {
+  try {
+      const query = this.getFilter();
+      const hasBook = await Book.exists({ author: query._id });
+
+      if (hasBook) {
+          next(new Error("This author still has books."));
+      } else {
+          next();
+      }
+  } catch (err) {
+      next(err);
+  }
+});
 
 module.exports = mongoose.model('Author', authorSchema)
